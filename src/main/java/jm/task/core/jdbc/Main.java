4b739b727ff4b1409d66ed6c4c4698ea7fff6080
Flugeggehaimen <bliznyuk10@mail.ru>
2022-07-02 17:17:37 +0300

    Del unused import in Main class

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,24 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
 
-        userDao.saveUser("Ivan", "Ivanov", (byte) 13);
-        userDao.saveUser("Oleg", "Svetlov", (byte) 34);
-        userDao.saveUser("Alexei", "Petrov", (byte) 21);
-        userDao.saveUser("Petr", "Ivanov", (byte) 43);
+        userService.saveUser("Ivan", "Ivanov", (byte) 13);
+        userService.saveUser("Oleg", "Svetlov", (byte) 34);
+        userService.saveUser("Alexei", "Petrov", (byte) 21);
+        userService.saveUser("Petr", "Ivanov", (byte) 43);
 
-        System.out.println(userDao.getAllUsers());
+        System.out.println(userService.getAllUsers());
 
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
         Util.close();
     }
 }
